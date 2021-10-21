2959ad19985d4351eee4ed1f31295f1a35bb3fd7
Sebastian <gutgemann@gmail.com>
2021-10-21 13:16:02 +0200

    Added script to execture extraction functions

---
+++ 3_Code/10_Run_a4d_tracker_extraction.R
@@ -0,0 +1,26 @@
+## Example script to process a tracker file
+## TODO: Enhance with upload to database
+
+
+## Set wd equal path of repository
+setwd("D:/Work/Correlaid/A4D/a4d_analytics")
+
+# Tracker path
+example_tracker_path <- "2_Data/2017_Tracker.xlsx"
+
+# Setup
+source("3_Code/a4d_tracker_function.R")
+source("3_Code/a4d_tracker_functionii.R")
+
+
+
+### Data extraction
+df_raw <- reading_a4d_tracker(tracker_data_file = example_tracker_path,
+                                 year = 2017,
+                                 clinic = "ABC",
+                                 country = "DEF")
+
+
+### Data cleanse
+df_cleaned <- cleaning_a4d_tracker(data = df_raw)
+View(df_raw)
