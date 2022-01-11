42b5b838588134c72a6a505f79af93715d09f6cc
paniterka <teresa.kubacka+github@gmail.com>
2022-01-11 23:22:05 +0100

    refactor script to read in the tracker, add new tab to the codebook

--- 3_Code/10_Run_a4d_tracker_extraction.R
+++ 3_Code/10_Run_a4d_tracker_extraction.R
@@ -3,22 +3,22 @@
 
 
 ## Set wd equal path of repository
-setwd("D:/Work/Correlaid/A4D/a4d_analytics")
+setwd("D:/a4d_code/a4d_analytics")
 
 # Tracker path
-example_tracker_path <- "2_Data/2021_Tracker.xlsx"
+example_tracker_path <- "d:/a4d_data/01_2017 AN Clinic IX A4D Tracker (1).xlsx"
+# example_tracker_path <- "2_Data/2017_Tracker.xlsx"
+
+codebook_path <- "4ADMonthlyTrackerCodebook.xlsx"
 
 # Setup
 source("3_Code/01_a4d_tracker_extract.R")
 source("3_Code/02_a4d_tracker_format.R")
 
-
+codebook <- read_column_synonyms(codebook_path)
 
 ### Data extraction
-df_raw <- reading_a4d_tracker(tracker_data_file = example_tracker_path,
-                                 year = 2021,
-                                 clinic = "ABC",
-                                 country = "DEF")
+df_raw <- reading_a4d_tracker(tracker_data_file = example_tracker_path, codebook = codebook)
 
 
 ### Data cleanse
