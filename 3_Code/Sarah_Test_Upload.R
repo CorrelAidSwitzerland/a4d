6885e7b49503e6bb78aa1758ac93c93527d0c87e
Michael Aydinbas <michael.aydinbas@gmail.com>
2023-03-29 23:40:35 +0200

    delete old unused code and test files

--- 3_Code/Sarah_Test_Upload.R
+++
@@ -1,5 +0,0 @@
----------------- # Datacross - A4D Project: Phase 1 ------------- #
----------------- # Preprocessing: Medical Supply Data (MSD) ----- #
----------------- # Test for Git upload -------------------------- #   
-
-# Hello guys, first test if pushing this file to the git works. If you can read this, I'm happy. Cheers, Sarah. 
