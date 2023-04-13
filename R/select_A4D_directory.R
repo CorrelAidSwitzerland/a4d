30a9468cb47b83f69975f3b1f59e298db434b5b8
YannLadner <yann_ladi@hotmail.com>
2023-04-14 00:52:18 +0200

    Re-wrote select_A4D_directory() Improved function and updated NAMESPACE

--- R/select_A4D_directory.R
+++ R/select_A4D_directory.R
@@ -1,20 +1,20 @@
 #' Selects path to A4D data and sets it as an environment variable
 #'
-#' @return Sets the env variable to A4D data folder
+#' @return Returns a character representing the path to the tracker data
 #' @export
 #'
-select_A4D_directory <- function() {
+#' @param reset A boolean. If set to TRUE, the directory containing the tracker
+#' data is changed.
+#'
+select_A4D_directory <- function(reset = FALSE) {
     a4d_data_root <- Sys.getenv("A4D_DATA_ROOT")
     if (a4d_data_root == "") {
-        answer <- readline(prompt = cat('\nDo you want to set an env variable for the A4D data folder? (write "y/n" and press enter):\n'))
-
-        if (answer == "y") {
-            cat("Navigate to the directory")
-            a4d_data_root <- rstudioapi::selectDirectory()
-            Sys.setenv(A4D_DATA_ROOT = a4d_data_root)
-
-            cat("\n\nA4D data folder set to:", a4d_data_root, "\n")
-        }
+        a4d_data_root <- set_a4d_data_root()
     }
+
+    if (reset) {
+        a4d_data_root <- set_a4d_data_root()
+    }
+
     return(a4d_data_root)
 }
