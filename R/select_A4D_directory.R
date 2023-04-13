8fd884d4baab7c60b3dc295bf1d2edce8b90720f
YannLadner <yann_ladi@hotmail.com>
2023-04-14 01:51:28 +0200

    Update functions

--- R/select_A4D_directory.R
+++ R/select_A4D_directory.R
@@ -1,20 +1,16 @@
-#' Selects path to A4D data and sets it as an environment variable
+#' Selects path to A4D data and sets it as an environment variable.
 #'
-#' @return Returns a character representing the path to the tracker data
 #' @export
 #'
-#' @param reset A boolean. If set to TRUE, the directory containing the tracker
+#' @param reset A boolean. If set to TRUE, the directory containing the tracker.
 #' data is changed.
 #'
+#' @return Returns a character representing the path to the tracker data.
+#'
 select_A4D_directory <- function(reset = FALSE) {
-    a4d_data_root <- Sys.getenv("A4D_DATA_ROOT")
-    if (a4d_data_root == "") {
-        a4d_data_root <- set_a4d_data_root()
-    }
-
-    if (reset) {
-        a4d_data_root <- set_a4d_data_root()
-    }
-
-    return(a4d_data_root)
+  a4d_data_root <- Sys.getenv("A4D_DATA_ROOT")
+  if (reset || a4d_data_root == "") {
+    a4d_data_root <- set_a4d_data_root()
+  }
+  return(a4d_data_root)
 }
