615a1091b661cc3eee7155d3f125dd09129e658b
YannLadner <yann_ladi@hotmail.com>
2023-04-14 00:23:34 +0200

    Create helper function to select env var

---
+++ R/set_a4d_data_root.R
@@ -0,0 +1,13 @@
+#' Helper function that sets the env variable to the A4D tracker files
+#'
+#' @return Returns a character representing the path to the tracker data
+#' @export
+#'
+set_a4d_data_root <- function() {
+  cat("Select the directory containing the tracker files")
+  a4d_data_root <- rstudioapi::selectDirectory()
+  Sys.setenv(A4D_DATA_ROOT = a4d_data_root)
+
+  cat("\n\nA4D data folder set to:", a4d_data_root, "\n")
+  return(a4d_data_root)
+}
