53e71936d2cbf4b84b64894ea1fc8681fb53b894
Michael Aydinbas <michael.aydinbas@gmail.com>
2023-05-30 11:24:44 +0200

    add export to main script

--- run_script_1_patient_data.R
+++ run_script_1_patient_data.R
@@ -4,28 +4,25 @@ tracker_root_path <- select_A4D_directory()
 
 # Only for single files, for now, for easier testing
 # Loop will be implemented later
-tracker_file <- rstudioapi::selectFile(path = tracker_root_path, filter = "Excel Workbook (*.xlsx)")
+tracker_file <-
+    rstudioapi::selectFile(path = tracker_root_path, filter = "Excel Workbook (*.xlsx)")
 
 
 codebook_path <- "master_tracker_variables.xlsx"
 
 ## Extract codebooks for each data form
-codebook_patient <- read_column_synonyms(
-    codebook_path, sheet = "synonyms_PatientData"
-)
+codebook_patient <-
+    read_column_synonyms(codebook_path, sheet = "synonyms_PatientData")
 
-codebook_product <- read_column_synonyms(
-    codebook_path, sheet = "synonyms_ProductData"
-)
+codebook_product <-
+    read_column_synonyms(codebook_path, sheet = "synonyms_ProductData")
 
 columns_synonyms = codebook_patient
 
 
 # Use new read function ---------------------------------------------------
-df_raw <- reading_patient_data_2(
-    tracker_data_file = tracker_file,
-    columns_synonyms = codebook_patient
-)
+df_raw <- reading_patient_data_2(tracker_data_file = tracker_file,
+                                 columns_synonyms = codebook_patient)
 
 
 # Combine to a new data frame ---------------------------------------------
@@ -34,36 +31,34 @@ df_raw %>% dim # quickly check dimensions
 
 # INCOMPLETE - Set sensitive rows to NA -------------------------------------
 # level of education is in the patient list - we need to get data from there as well
-df_raw_comb <-
-    df_raw_comb %>%
-    mutate(
-        across(
-            c(
-            patient_name,
-            country_code,
-            clinic_code,
-            # etc.
-            ),
-            ~NA
-        ))
+df_raw <-
+    df_raw %>%
+    mutate(across(c(
+        patient_name,
+        province,
+        dob,
+        country_code,
+        clinic_code,
+        # etc.
+    ),
+    ~ NA))
 
 
 # INCOMPLETE - Store data ------------------------------------------------------
 
-extracted_root <-  file.path(
-    tracker_root_path,
-    "Extracted files"
-)
+extracted_root <-  file.path(tracker_root_path,
+                             "output",
+                             "sensitive_data_removed")
 
 if (!file.exists(extracted_root)) {
     dir.create(extracted_root)
 }
 
 
-df_raw_comb %>%
+df_raw %>%
     write.csv(file =
-                  paste0(extracted_root,
-                         "/",
-                         basename(tracker_file),
-                         "_extracted.csv"),
+                  file.path(
+                      extracted_root,
+                      paste0(tools::file_path_sans_ext(basename(tracker_file)), "_extracted.csv")
+                  ),
               row.names = F)
