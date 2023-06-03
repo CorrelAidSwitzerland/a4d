81f8e86d1a2f8de9e235e1da1d61ac5d73a83dda
YannLadner <yann_ladi@hotmail.com>
2023-06-03 12:31:55 +0200

    Update tests for read_column_synonyms

--- tests/testthat/test-read_column_synonyms.R
+++ tests/testthat/test-read_column_synonyms.R
@@ -1,16 +1,17 @@
 test_that("read_column_synonyms works", {
-    master_file <- test_path("testdata", "synonyms.xlsx")
 
     patient_synonyms <- read_column_synonyms(
-        master_file, sheet = "synonyms_PatientData"
+        "synonyms_patient.yaml"
     )
 
     expect_equal(colnames(patient_synonyms), c("variable_name", "tracker_name"))
 
     age_synonyms = patient_synonyms %>% dplyr::filter(variable_name=="age")
-    expect_setequal(age_synonyms$tracker_name, c("Age", "Age*"))
+    expect_setequal(age_synonyms$tracker_name, c("Age", "Age*", "age on reporting", "Age (Years)"))
 
-    baseline_synonyms = patient_synonyms %>% dplyr::filter(variable_name=="baseline_fbg_mgdl")
-    expect_setequal(baseline_synonyms$tracker_name, c("FBG (mg/dL) - BASELINE", "Baseline SMBG or CBG (mg/dL)", "Baseline FBG (mg/dL)"))
+    baseline_synonyms = patient_synonyms %>% dplyr::filter(grepl(variable_name, pattern = "baseline_fbg"))
+    expect_setequal(baseline_synonyms$tracker_name, c("FBG (mg/dL) - BASELINE", "Baseline SMBG or CBG (mg/dL)", "Baseline FBG (mg/dL)",
+                                                      "Baseline FBG (mmol/L)", "Baseline FBG (mg%)", "Baseline FBG or CBG (mg/dL)",
+                                                      "baseline_fbg_unit"))
 
 })
