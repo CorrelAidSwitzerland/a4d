ae94089d9f55da8c1ff619fc4b4a8dc31cf14403
Michael Aydinbas <michael.aydinbas@gmail.com>
2023-08-01 08:48:50 +0200

    fix problems with renaming patient_id -> id

--- R/helper_read_patient_data.R
+++ R/helper_read_patient_data.R
@@ -2,14 +2,14 @@
 # expects that patient ID has a certain format
 extract_country_clinic_code <- function(patient_data) {
     logDebug("Start extract_country_clinic_code.")
-    patient_ids <- patient_data["patient_id"] %>%
-        dplyr::filter(patient_id != "0") %>%
+    patient_ids <- patient_data["id"] %>%
+        dplyr::filter(id != "0") %>%
         drop_na() %>%
         rowwise() %>%
         mutate(
-            country = str_split(patient_id, "_", n = 2, simplify = T)[1],
+            country = str_split(id, "_", n = 2, simplify = T)[1],
             clinic = substr(str_split(
-                patient_id, "_",
+                id, "_",
                 n = 2, simplify = T
             )[2], 0, 2)
         )
