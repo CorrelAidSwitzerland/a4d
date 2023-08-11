af889c14eb5c308bc1e87c1b346e7c38203d7dd4
Michael Aydinbas <michael.aydinbas@gmail.com>
2023-08-12 00:10:29 +0200

    add fix_bmi and fix_height

--- run_script_2_clean_data.R
+++ run_script_2_clean_data.R
@@ -266,12 +266,15 @@ process_patient_file <- function(paths, patient_file, patient_file_name, output_
         rowwise() %>%
         # 3. fix remaining problems in the target data type
         mutate(
-            bmi = fix_bmi(bmi, weight, height, id) %>% cut_numeric_value(min = 4, max = 60, "bmi"),
-            age = fix_age(age, dob, tracker_year, tracker_month, id), # fix DOB first!
+            height = transform_cm_to_m(height) %>% cut_numeric_value(min=0, max=2.3)
+            bmi = fix_bmi(weight, height, id) %>% cut_numeric_value(min = 4, max = 60, "bmi"), # fix height and weight first
+            age = fix_age(age, dob, tracker_year, tracker_month, id) %>% cut_numeric_value(min=0, max=25, "age"), # fix DOB first!
             gender = fix_gender(gender, id),
-            hba1c_baseline = cut_numeric_value(hba1c_baseline, 4, 18, "hba1c_baseline"),
-            fbg_baseline_mmol = cut_numeric_value(fbg_baseline_mmol, 0, 136.5, "fbg_baseline_mmol"), # https://www.cleveland19.com/story/1425584/ohio-man-holds-world-record-of-highest-blood-sugar/
-            fbg_updated_mmol = cut_numeric_value(fbg_baseline_mmol, 0, 136.5, "fbg_updated_mmol") # https://www.cleveland19.com/story/1425584/ohio-man-holds-world-record-of-highest-blood-sugar/
+            hba1c_baseline = cut_numeric_value(hba1c_baseline, min=4, max=18, "hba1c_baseline"),
+            fbg_baseline_mmol = cut_numeric_value(fbg_baseline_mmol, min=0, max=136.5, "fbg_baseline_mmol"), # https://www.cleveland19.com/story/1425584/ohio-man-holds-world-record-of-highest-blood-sugar/
+            fbg_updated_mmol = cut_numeric_value(fbg_baseline_mmol, min=0, max=136.5, "fbg_updated_mmol"), # https://www.cleveland19.com/story/1425584/ohio-man-holds-world-record-of-highest-blood-sugar/
+            blood_pressure_sys_mmhg = cut_numeric_value(blood_pressure_sys_mmhg, min=20, max=250, "blood_pressure_sys_mmhg"),
+            blood_pressure_dias_mmhg = cut_numeric_value(blood_pressure_dias_mmhg, min=20, max=220, "blood_pressure_dias_mmhg")
         ) %>%
         ungroup()
 
