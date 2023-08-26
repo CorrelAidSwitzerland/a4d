ed82f49051d7ddfdfb2097975953f57934df1292
Oleg T <tvoleg2@gmail.com>
2023-08-26 09:32:55 +0200

    check negative values in product_balance column

--- R/read_product_data.R
+++ R/read_product_data.R
@@ -143,6 +143,23 @@ check_entry_dates <- function(df, Sheet) {
     }
 }
 
+# Check negative values in product_balance column
+check_negative_balance <- function(df, Sheet) {
+    # Create a new data frame containing only rows with negative values in product_balance column
+    negative_df <- df[df$product_balance < 0, ]
+
+    # Check if there are any rows in the new data frame
+    if (nrow(negative_df) > 0) {
+        # Log a warning message with the number of negative values and their corresponding product_balance values
+        logWarn(
+            Sheet,
+            " number of negative values in product_balance on the sheet is ",
+            nrow(negative_df), ": ",
+            paste(negative_df$product_balance, collapse = ", ")
+        )
+    }
+}
+
 #' @title Remove Rows with NA Values in Specified Columns.
 #'
 #' @description
@@ -266,6 +283,9 @@ reading_product_data_step2 <-
 
             # remove index column
             product_df <- subset(product_df, select = -index)
+
+            # check negative values in product_balance column
+            check_negative_balance(product_df, sheet_month)
 
             #### hospital and country information missing here!!
 
