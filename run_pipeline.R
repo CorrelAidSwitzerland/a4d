e94b3f8ef985873addb32db69a0dcbee3bb1735a
Michael Aydinbas <michael.aydinbas@gmail.com>
2023-09-29 07:42:13 +0000

    adjust script3 to run on GCP VM with a4d specific project settings; tested on VM and data was successfully ingested into the database

--- run_pipeline.R
+++ run_pipeline.R
@@ -1,5 +1,5 @@
-Sys.setenv(A4D_DATA_ROOT = "/Volumes/USB SanDisk 3.2Gen1 Media/a4d") # lokal
-# Sys.setenv(A4D_DATA_ROOT="/home/rstudio/data") # on GCP VM
+# Sys.setenv(A4D_DATA_ROOT = "/Volumes/USB SanDisk 3.2Gen1 Media/a4d") # lokal
+Sys.setenv(A4D_DATA_ROOT="/home/rstudio/data") # on GCP VM
 BUCKET_DOWLOAD <- "a4dphase2_upload"
 BUCKET_UPLOAD <- "a4dphase2_output"
 PROJECT_ID <- "a4d-315220"
@@ -33,7 +33,7 @@ ingest_data <- function(project_id, dataset, table, source, schema) {
         "bq load",
         "--source_format=CSV",
         "--encoding=UTF-16LE",
-        "--project_id=", project_id,
+        paste0("--project_id=", project_id),
         "--skip_leading_rows=1",
         "--max_bad_records=0",
         "--allow_quoted_newlines=true",
@@ -51,12 +51,13 @@ ingest_data <- function(project_id, dataset, table, source, schema) {
 }
 
 data_dir <- select_A4D_directory()
-table_dir <- file.path(data_dir, BUCKET_DOWLOAD, "output", "tables")
+output_dir <- file.path(data_dir, "output")
+table_dir <- file.path(output_dir, "tables")
 download_data(bucket = BUCKET_DOWLOAD, data_dir = data_dir)
 source("run_script_1_extract_raw_data.R") # creates CSV files in subfolders patient_data_raw and product_data_raw
 source("run_script_2_clean_data.R") # creates CSV files in subfolders patient_data_cleaned and product_data_cleaned
 source("run_script_3_create_tables.R") # creates final CSV files in subfolder tables
-upload_data(bucket = BUCKET_UPLOAD, data_dir = file.path(data_dir, BUCKET_DOWLOAD, "output"))
+upload_data(bucket = BUCKET_UPLOAD, data_dir = output_dir)
 ingest_data(
     project_id = PROJECT_ID,
     dataset = DATASET,
