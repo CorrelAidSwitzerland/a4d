5b6cc1db5f0408b629c7a6319801a040c5051ed5
Oleg T <tvoleg2@gmail.com>
2023-10-04 13:43:17 +0200

    add reorder_product_fields in script 3

--- R/create_table_product_data.R
+++ R/create_table_product_data.R
@@ -1,12 +1,16 @@
 #' @title Create a Single CSV for Table Product Data
 #'
 #' @description
-#' This function reads all CSV files in a specified directory, merges them into a single data frame, and writes the merged data to a new CSV file. If there are any columns that are present in some CSV files but not others, this function will add those missing columns to the data frames where they are missing and fill them with NA.
+#' This function reads all CSV files in a specified directory, merges them into a single data frame,
+#' and writes the merged data to a new CSV file with reordered columns according to the list of fields.
+#' If there are any columns that are present in some CSV files but not others,
+#' this function will add those missing columns to the data frames where they are missing and fill them with NA.
 #'
 #' @param input_root A string specifying the path to the directory containing the input CSV files.
 #' @param output_root A string specifying the path to the directory where the output CSV file should be written.
 #'
-#' @return This function does not return a value. It writes the merged data to a new CSV file in the output_root directory.
+#' @return This function does not return a value. It writes the merged data to a new CSV file
+#' (with reordered columns according to the list of fields) in the output_root directory.
 #'
 #' @examples
 #' create_table_product_data("path/to/input/directory", "path/to/output/directory")
@@ -33,6 +37,9 @@ create_table_product_data <- function(input_root, output_root) {
     # Merge all data frames
     merged_data <- do.call(rbind, data_list)
 
+    # Reorder the columns according to the list of fields
+    merged_data <- reorder_product_fields(merged_data)
+
     # Write the merged data to a CSV file in the output_root directory
     export_data(
         data = merged_data,
@@ -42,4 +49,59 @@ create_table_product_data <- function(input_root, output_root) {
     )
 
     logInfo("Finish creating single csv for table product_data.")
+}
+
+
+#' @title Reorder and Add Missing Fields in a Dataframe
+#'
+#' @description
+#' This function checks if all specified fields are present in the input dataframe.
+#' If any fields are missing, it adds them with NA values.
+#' It then reorders the columns according to the specified list of fields.
+#' Any additional fields in the input dataframe are moved to the end.
+#'
+#' @param merged_data A dataframe that needs its columns to be reordered and missing fields added.
+#'
+#' @return A dataframe with reordered columns and added missing fields.
+#'
+#' @examples
+#' \dontrun{
+#' df <- data.frame(product = c("A", "B"), product_units_notes = c("note1", "note2"))
+#' df <- reorder_product_fields(df)
+#' }
+#'
+reorder_product_fields <- function(merged_data) {
+    # List of fields
+    fields <- c(
+        "product",
+        "product_units_notes",
+        "product_entry_date",
+        "product_units_released",
+        "product_released_to",
+        "product_units_received",
+        "product_received_from",
+        "product_balance",
+        "product_units_returned",
+        "product_returned_by",
+        "product_table_month",
+        "product_table_year",
+        "product_sheet_name",
+        "file_name",
+        "product_balance_status",
+        "product_country",
+        "product_hospital"
+    )
+
+    # Check if all fields are present in merged_data
+    missing_fields <- setdiff(fields, names(merged_data))
+
+    # If there are missing fields, add them with NA
+    if (length(missing_fields) > 0) {
+        merged_data[missing_fields] <- NA
+    }
+
+    # Reorder the columns according to the list of fields
+    merged_data <- merged_data[, c(fields, setdiff(names(merged_data), fields))]
+
+    return(merged_data)
 }
