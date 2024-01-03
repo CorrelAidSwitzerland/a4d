bc15cd71ae345786e18105695cb0057a3be8807b
Luke Boelling <lboel@users.noreply.github.com>
2024-01-03 11:23:32 +0100

    Update Namespace Run Styler on helper Main and script 3
    Export function to convert csv to parquet for static clinic data

--- scripts/run_script_3_create_tables.R
+++ scripts/run_script_3_create_tables.R
@@ -11,117 +11,117 @@ patient_data_files <- a4d::get_files(file.path(paths$output_root, "patient_data_
 product_data_files <- a4d::get_files(file.path(paths$output_root, "product_data_cleaned"), pattern = "\\.parquet$")
 
 logInfo(
-    "Found ",
-    length(patient_data_files),
-    " patient csv files under ",
-    paths$tracker_root,
-    "."
+  "Found ",
+  length(patient_data_files),
+  " patient csv files under ",
+  paths$tracker_root,
+  "."
 )
 logInfo(
-    "Found ",
-    length(product_data_files),
-    " product csv files under ",
-    paths$tracker_root,
-    "."
+  "Found ",
+  length(product_data_files),
+  " product csv files under ",
+  paths$tracker_root,
+  "."
 )
 
 logInfo("Start creating table csv files.")
 
 logfile <- "table_patient_data_static"
 with_file_logger(logfile,
-    {
-        tryCatch(
-            {
-                a4d::create_table_patient_data_static(patient_data_files, file.path(paths$output_root, "patient_data_cleaned"), paths$tables)
-            },
-            error = function(e) {
-                logError("Could not create table csv for static patient data. Error: ", e$message)
-            },
-            warning = function(w) {
-                logWarn("Could not create table csv for static patient data. Error: ", w$message)
-            }
-        )
-    },
-    output_root = paths$output_root
+  {
+    tryCatch(
+      {
+        a4d::create_table_patient_data_static(patient_data_files, file.path(paths$output_root, "patient_data_cleaned"), paths$tables)
+      },
+      error = function(e) {
+        logError("Could not create table csv for static patient data. Error: ", e$message)
+      },
+      warning = function(w) {
+        logWarn("Could not create table csv for static patient data. Error: ", w$message)
+      }
+    )
+  },
+  output_root = paths$output_root
 )
 
 logfile <- "table_patient_data_monthly"
 with_file_logger(logfile,
-    {
-        tryCatch(
-            {
-                a4d::create_table_patient_data_monthly(patient_data_files, file.path(paths$output_root, "patient_data_cleaned"), paths$tables)
-            },
-            error = function(e) {
-                logError("Could not create table csv for monthly patient data. Error: ", e$message)
-            },
-            warning = function(w) {
-                logWarn("Could not create table csv for monthly patient data. Error: ", w$message)
-            }
-        )
-    },
-    output_root = paths$output_root
+  {
+    tryCatch(
+      {
+        a4d::create_table_patient_data_monthly(patient_data_files, file.path(paths$output_root, "patient_data_cleaned"), paths$tables)
+      },
+      error = function(e) {
+        logError("Could not create table csv for monthly patient data. Error: ", e$message)
+      },
+      warning = function(w) {
+        logWarn("Could not create table csv for monthly patient data. Error: ", w$message)
+      }
+    )
+  },
+  output_root = paths$output_root
 )
 
 logfile <- "table_longitudinal_data_hba1c"
 with_file_logger(logfile,
-    {
-        tryCatch(
-            {
-                a4d::create_table_longitudinal_data(
-                    patient_data_files,
-                    file.path(paths$output_root, "patient_data_cleaned"),
-                    paths$tables,
-                    "hba1c_updated",
-                    "hba1c"
-                )
-            },
-            error = function(e) {
-                logError("Could not create table csv for longitudinal patient data. Error: ", e$message)
-            },
-            warning = function(w) {
-                logWarn("Could not create table csv for longitudinal patient data. Error: ", w$message)
-            }
+  {
+    tryCatch(
+      {
+        a4d::create_table_longitudinal_data(
+          patient_data_files,
+          file.path(paths$output_root, "patient_data_cleaned"),
+          paths$tables,
+          "hba1c_updated",
+          "hba1c"
         )
-    },
-    output_root = paths$output_root
+      },
+      error = function(e) {
+        logError("Could not create table csv for longitudinal patient data. Error: ", e$message)
+      },
+      warning = function(w) {
+        logWarn("Could not create table csv for longitudinal patient data. Error: ", w$message)
+      }
+    )
+  },
+  output_root = paths$output_root
 )
 
-    logfile <- "clinic_data_static"
-    with_file_logger(logfile,
-                     {
-                         tryCatch(
-                             {
-                                 a4d::export_data_as_parquet(data = read.csv("reference_data/clinic_data_static.csv"),filename = "clinic_data_static",output_root = paths$tables,suffix="")
-                             },
-                             error = function(e) {
-                                 logError("Could not create clinic data static table. Error: ", e$message)
-                             },
-                             warning = function(w) {
-                                 logWarn("Could not create clinic data static table. Warning: ", w$message)
-                             }
-                         )
-                     },
-                     output_root = paths$output_root
+logfile <- "clinic_data_static"
+with_file_logger(logfile,
+  {
+    tryCatch(
+      {
+        a4d::export_data_as_parquet(data = read.csv("reference_data/clinic_data_static.csv"), filename = "clinic_data_static", output_root = paths$tables, suffix = "")
+      },
+      error = function(e) {
+        logError("Could not create clinic data static table. Error: ", e$message)
+      },
+      warning = function(w) {
+        logWarn("Could not create clinic data static table. Warning: ", w$message)
+      }
     )
+  },
+  output_root = paths$output_root
+)
 
 
 logfile <- "table_product_data"
 with_file_logger(logfile,
-    {
-        tryCatch(
-            {
-                a4d::create_table_product_data(file.path(paths$output_root, "product_data_cleaned"), paths$tables)
-            },
-            error = function(e) {
-                logError("Could not create table for product data. Error: ", e$message)
-            },
-            warning = function(w) {
-                logWarn("Could not create table for product data. Warning: ", w$message)
-            }
-        )
-    },
-    output_root = paths$output_root
+  {
+    tryCatch(
+      {
+        a4d::create_table_product_data(file.path(paths$output_root, "product_data_cleaned"), paths$tables)
+      },
+      error = function(e) {
+        logError("Could not create table for product data. Error: ", e$message)
+      },
+      warning = function(w) {
+        logWarn("Could not create table for product data. Warning: ", w$message)
+      }
+    )
+  },
+  output_root = paths$output_root
 )
 
 logInfo("Finish creating table files.")
@@ -131,24 +131,23 @@ logInfo("Trying to link files for product and patient data.")
 logfile <- "link_product_patient_data"
 
 with_file_logger(logfile,
-    {
-        tryCatch(
-            {
-                a4d::link_product_patient(
-                    file.path(paths$tables, "product_data.parquet"),
-                    file.path(paths$tables, "patient_data_monthly.parquet")
-                )
-            },
-            error = function(e) {
-                logError("Could not link files for product and patient data. Error: ", e$message)
-            },
-            warning = function(w) {
-                logWarn("Could not link files for product and patient data. Warning: ", w$message)
-            }
+  {
+    tryCatch(
+      {
+        a4d::link_product_patient(
+          file.path(paths$tables, "product_data.parquet"),
+          file.path(paths$tables, "patient_data_monthly.parquet")
         )
-    },
-    output_root = paths$output_root
+      },
+      error = function(e) {
+        logError("Could not link files for product and patient data. Error: ", e$message)
+      },
+      warning = function(w) {
+        logWarn("Could not link files for product and patient data. Warning: ", w$message)
+      }
+    )
+  },
+  output_root = paths$output_root
 )
 
 logInfo("Finished linking files for product and patient data.")
-
