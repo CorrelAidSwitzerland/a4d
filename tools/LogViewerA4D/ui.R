1f331e51dab33f3afea8b9d30adaca2a7cae3ba3
Luke Boelling <lboel@users.noreply.github.com>
2024-02-14 19:51:05 +0100

    linting

--- tools/LogViewerA4D/ui.R
+++ tools/LogViewerA4D/ui.R
@@ -11,69 +11,73 @@
 shinyUI(
   dashboardPage(
     dashboardHeader(title = "Log File Viewer - A4D"),
-    dashboardSidebar( fluidRow(
-      uiOutput( "loadTempFileUI"),
-             fileInput(
-               "fileUpload",
-               "Upload Log Files",
-               multiple = TRUE, accept = ".log")),
-
-
-    sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Overview", tabName = "overview", icon = icon("th")),
-      menuItem("Reference_Data", tabName = "ref_data", icon = icon("clipboard"))
-    )),
+    dashboardSidebar(
+      fluidRow(
+        uiOutput("loadTempFileUI"),
+        fileInput(
+          "fileUpload",
+          "Upload Log Files",
+          multiple = TRUE, accept = ".log"
+        )
+      ),
+      sidebarMenu(
+        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("Overview", tabName = "overview", icon = icon("th")),
+        menuItem("Reference_Data", tabName = "ref_data", icon = icon("clipboard"))
+      )
+    ),
     dashboardBody(
       tabItems(
-      tabItem(tabName = "dashboard",
-
-      fluidRow(
-        box(width = 6,
-          title = "Main Table",
-        DTOutput("logTable")),
-        box(width = 6,
-            title = "Tracker Overview",
-            DTOutput("trackerSummary"),
-            plotlyOutput("sankey")),
-
-
-
-      )),
-      tabItem(tabName = "overview",
-              fluidRow(box(
-                title = "Overview",
-              DTOutput("status")
-              )),
-              fluidRow(box(title= "Overview-Plot", height=1000, weight=1000,
-                           textInput("regexFilter",label = "RegexFilter Trackerfiles"),
-                  plotlyOutput( "logOverviewPlot")))
-      ),
-
-      tabItem(tabName = "ref_data",
-              fluidPage(
-                  sidebarLayout(
-                      sidebarPanel(
-                          h3("Checking reference_data"),
-                          br(),
-                          p("We use reference_data to match clinic information
+        tabItem(
+          tabName = "dashboard",
+          fluidRow(
+            box(
+              width = 6,
+              title = "Main Table",
+              DTOutput("logTable")
+            ),
+            box(
+              width = 6,
+              title = "Tracker Overview",
+              DTOutput("trackerSummary"),
+              plotlyOutput("sankey")
+            ),
+          )
+        ),
+        tabItem(
+          tabName = "overview",
+          fluidRow(box(
+            title = "Overview",
+            DTOutput("status")
+          )),
+          fluidRow(box(
+            title = "Overview-Plot", height = 1000, weight = 1000,
+            textInput("regexFilter", label = "RegexFilter Trackerfiles"),
+            plotlyOutput("logOverviewPlot")
+          ))
+        ),
+        tabItem(
+          tabName = "ref_data",
+          fluidPage(
+            sidebarLayout(
+              sidebarPanel(
+                h3("Checking reference_data"),
+                br(),
+                p("We use reference_data to match clinic information
                           to clinic_code used in messages."),
-                          radioButtons("choose_tab",
-                                             label = "Choose Output",
-                                             choices = c("all", "missing entries", "multiple entries"),
-                                             selected = "all"
-                                             )
-                      ),
-                      mainPanel(
-                          DTOutput("ref_data_df")
-
-                      )
-              ))
-
+                radioButtons("choose_tab",
+                  label = "Choose Output",
+                  choices = c("all", "missing entries", "multiple entries"),
+                  selected = "all"
+                )
+              ),
+              mainPanel(
+                DTOutput("ref_data_df")
               )
-      ))
-
+            )
+          )
+        )
       )
     )
-
-
+  )
+)
